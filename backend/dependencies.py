6d6eadab1a83e0193c95ffe5c5eb6e1611551e17
Tomaz03 <e.ftomaz@gmail.com>
2025-08-11 07:07:24 -0300

    Delete backend directory

--- backend/dependencies.py
+++
@@ -1,8 +0,0 @@
-# backend/dependencies.py
-from fastapi import Depends
-from .database import get_db
-from . import models
-
-def get_current_user():
-    # Função simulada — substitua pela real
-    return models.User(id=1, username="Usuário Simulado")
